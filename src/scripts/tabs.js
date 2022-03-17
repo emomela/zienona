a9762bc2d217ddc3a6276371046038212a0755f0
emomela <86851827+emomela@users.noreply.github.com>
2022-03-17 17:13:33 +0100

    d

--- src/scripts/tabs.js
+++ src/scripts/tabs.js
@@ -34,38 +34,6 @@ jQuery(document).ready(function($){
   //$(selector).Tabs();
 });
 
-$.fn.Tabs = function() {
-	var selector = this;
-
-	this.each(function() {
-		var obj = $(this); 
-		$(obj.attr('href')).hide();
-		$(obj).click(function() {
-			$(selector).removeClass('selected');
-			
-			$(selector).each(function(i, element) {
-				$($(element).attr('href')).hide();
-			});
-			
-			$(this).addClass('selected');
-			$($(this).attr('href')).fadeIn();
-			return false;
-		});
-	});
-
-	$(this).show();
-	$(this).first().click();
-	if(location.hash!='' && $('a[href="' + location.hash + '"]').length)
-		$('a[href="' + location.hash + '"]').click();	
-};
-
-
 jQuery(document).ready(function($){
-
-  //использование функции табов:
-  //вызываем на блоке, содержащем ссылки для переключения блоков
-  //в href ссылок указываются id блоков, которые должны переключаться
-  $('.account-page-tab-items a').Tabs();
-  //если на странице несколько экземпляров табов, вызываем функцию для каждого
-  //$(selector).Tabs();
+  $('.account-page-tab-items a').Tabs(); 
 });
