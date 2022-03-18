85736199ee5cb5672e94110cfec800db1a027295
emomela <86851827+emomela@users.noreply.github.com>
2022-03-18 13:41:13 +0100

    d

--- src/scripts/counter.js
+++ src/scripts/counter.js
@@ -26,9 +26,44 @@ $(document).on('click', '.pr-card-amount-plus', function(){
 	});
 
     
-	$(document).on('change','.pr-card-amount-input',function(e){
+	$(document).on('change','.cart-item-amount-input',function(e){
 		let val = parseInt($(this).val(),10);
 		if (val < 0) {
 			$(this).val(0);
 		}
 	});
+
+
+$(document).on('click', '.cart-item-amount-plus', function(){
+		var thisInput = $(this).prev('input'),
+				thisInputVal = parseInt(thisInput.val(), 10);
+
+		if (isNaN(thisInputVal)) {
+			thisInput.val(1).change();
+		} else {
+			thisInput.val(parseInt(thisInput.val(), 10) + 1).change()
+		}
+	});
+
+	$(document).on('click', '.cart-item-amount-minus', function(){
+		var thisInput = $(this).next('input'),
+				thisInputVal = parseInt(thisInput.val(), 10);
+
+		if (isNaN(thisInputVal)) {
+			thisInput.val(0).change()
+		} else {
+		 if (thisInputVal < 1) {
+			 thisInput.val(0).change()
+		 } else {
+			 thisInput.val(parseInt(thisInput.val(), 10) - 1).change()
+		 }
+		}
+	});
+
+    
+	$(document).on('change','.cart-item-amount-input',function(e){
+		let val = parseInt($(this).val(),10);
+		if (val < 0) {
+			$(this).val(0);
+		}
+	});	
