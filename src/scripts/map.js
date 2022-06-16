908c6e48ec2c93e00466959ce798111ec4970576
emomela <86851827+emomela@users.noreply.github.com>
2022-06-16 03:41:36 +0200

    account page

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -6,7 +6,6 @@ function initMap() {
     zoom: 8,
   });
 }
-
 window.onload = initMap;
 
 
@@ -24,3 +23,34 @@ $ (function () {
   $('.dp-factura-block').toggleClass("chosen");
   })  
 });
+
+
+$(function() {
+  var $speed = 200; 
+  var $class = 'ol-ordered-product';
+  var $class_open = '.ol-ordered-special-active';
+  var $class_bg = '.ol-ordered-product-current';
+  var $class_hid = '.ol-ordered-special';
+
+  
+  $(document).ready(function() {
+     $('.ol-info').on('click', function() {
+       $ul = $(this).closest('ul');
+       $answer = $(this).closest('li').find($class_open);
+       
+       if( !$(this).closest('li').hasClass($class) ) {
+       
+         $ul.find('li').each(function() {
+           if( $(this).hasClass($class) )
+             $(this).removeClass($class).find($class_open).slideUp($speed);
+         });
+       }
+       
+       $answer
+         .slideToggle($speed)
+         .closest('li')
+         .toggleClass($class);
+     });
+  });
+});
+
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -4,11 +4,3 @@ $(document).ready(function() {
   $('.link-card-details').magnificPopup();
 });
 
-
-$(document).ready(function(){
-    $('.ol-info').on('click', function(){
-       console.log(111);
-     $('.ol-ordered-product').toggleClass('ol-ordered-product-current');
-     $('.ol-ordered-special').toggleClass('ol-ordered-special-active');
-  });
-});
